ae728644f133856dd172328a2991b8e4697736e3
Eliott Frilet <frilet.eliott@gmail.com>
2019-10-16 17:34:33 -0500

    Implement parseCookies middleware

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,14 @@
 const parseCookies = (req, res, next) => {
+  const cookieObject = {};
+  if (req.headers.cookie) {
+    let cookies = req.headers.cookie.split('; ');
+    cookies.forEach((cookie) => {
+      let cookieArray = cookie.split('=');
+      cookieObject[cookieArray[0]] = cookieArray[1];
+    });
+  }
+  req.cookies = cookieObject;
+  next();
 };
 
 module.exports = parseCookies;
