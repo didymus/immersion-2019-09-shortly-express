f1114e1a1d05f04ef1f74947e81d43ff7e8b38c9
Eliott Frilet <frilet.eliott@gmail.com>
2019-10-16 19:35:33 -0500

    Implement createSession case for having cookies

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,17 +2,26 @@ const models = require('../models');
 
 const createSession = (req, res, next) => {
   const cookies = req.cookies;
-  if (!Object.keys(cookies).length) {
+  // if we don't have cookies
+  if (Object.keys(cookies).length === 0) {
     models.Sessions.create()
       .then((insertResults) => {
         return models.Sessions.get({id: insertResults.insertId});
       })
       .then((sessionData) => {
+        req.session = sessionData;
         res.cookie('shortlyid', sessionData.hash);
+        next();
+      })
+      .catch(err => console.error(err));
+  } else { // if we have cookies
+    models.Sessions.get({hash: cookies.shortlyid})
+      .then((sessionData) => {
+        req.session = sessionData;
+        next();
       })
       .catch(err => console.error(err));
   }
-  next();
 };
 
 // Add additional authentication middleware functions below //
